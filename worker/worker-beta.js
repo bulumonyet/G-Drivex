2c520233b5d082d9c2b39e0b20689787422f1393
bulumonyet <77999468+bulumonyet@users.noreply.github.com>
2021-03-06 18:34:09 +0700

    Update worker-beta.js

--- worker/worker-beta.js
+++ worker/worker-beta.js
@@ -62,10 +62,11 @@ const uiConfig = {
 	"logo_height": "", // only if logo_image is true
 	"logo_width": "100px", // only if logo_image is true
 	"logo_link_name": "G-Drive", // if logo is true then link otherwise just text for name
-	"contact_link": "https://source-code.my.id", // Link to Contact Button on Menu
+	"contact_link": "
+	", // Link to Contact Button on Menu
 	"copyright_year": "2050", // year of copyright, can be anything like 2015 - 2020 or just 2020
 	"company_name": "C3-Cloud", // Name next to copyright
-	"company_link": "https://source-code.my.id", // link of copyright name
+	"company_link": "https://www.caxcox.com/", // link of copyright name
 	"credit": true, // Set this to true to give us credit
 	"display_size": true, // Set this to false to hide display file size
 	"display_time": false, // Set this to false to hide display modified time for folder and files
